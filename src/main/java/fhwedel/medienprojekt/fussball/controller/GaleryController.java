9a6e3b17058a6cec546e82ba60531ed6ec58a193
ellenschwartau <ellen.schwartau@hotmail.de>
2014-03-19 16:58:11 +0100

    Bildergalerie Upload

--- src/main/java/fhwedel/medienprojekt/fussball/controller/GaleryController.java
+++ src/main/java/fhwedel/medienprojekt/fussball/controller/GaleryController.java
@@ -1,8 +1,10 @@
 package fhwedel.medienprojekt.fussball.controller;
 
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class GaleryController {
@@ -22,7 +24,16 @@ public class GaleryController {
 	 */
 	@RequestMapping("/galerie/upload/")
 	public String displayGaleryImgUploadForm() {
-		
 		return "galeryImgUpload";
 	}
+	
+	/**
+	 * Speichert ein neues Bild in der Bildergalerie.
+	 */
+	@RequestMapping(method=RequestMethod.POST)
+	public String uploadImage(@RequestParam(value="image") MultipartFile image) {
+		
+		return "/galerie/";
+	}
+	
 }
