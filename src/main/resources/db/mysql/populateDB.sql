fd36509a40f076fe5d2b857d4099da34ce8827f5
ellenschwartau <ellen.schwartau@hotmail.de>
2014-03-20 16:05:34 +0100

    Datenbankskripte angepasst

--- src/main/resources/db/mysql/populateDB.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO team VALUES (1, 'James', 'Carter');
-INSERT INTO team VALUES (2, 'Helen', 'Leary');
-INSERT INTO team VALUES (3, 'Linda', 'Douglas');
-INSERT INTO team VALUES (4, 'Rafael', 'Ortega');
-INSERT INTO team VALUES (5, 'Henry', 'Stevens');
-INSERT INTO team VALUES (6, 'Sharon', 'Jenkins');
-
-INSERT INTO forum (date_time, author, topic, description, text, has_comments) VALUES (now(), 'Ellen', 'Testeintrag', 'Test', 'Das hier ist ein Test', false); 
