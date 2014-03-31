cdc4df44e5f43e17fdf58d333e341a18bb8f5fb2
ellenschwartau <ellen.schwartau@hotmail.de>
2014-03-31 15:57:46 +0200

    Fehlermeldungen

---
+++ src/main/java/fhwedel/medienprojekt/fussball/service/dataErrors/Constants.java
@@ -0,0 +1,15 @@
+package fhwedel.medienprojekt.fussball.service.dataErrors;
+
+/**
+ * Enthält Konstanten für die Datenbankarbeit.
+ * 
+ * @author Ellen Schwartau Minf9888
+ *
+ */
+public class Constants {
+	protected static final String dbReports = "report";
+	static final String dbForum = "forum";
+	static final String dbComments = "comments";
+	static final String dbPermissions = "permissions";
+	static final String dbUsers = "users";
+}
