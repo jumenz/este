ad6ad5d15f2d5a52a20961375a235990f76d8601
ellenschwartau <ellen.schwartau@hotmail.de>
2014-04-10 17:29:05 +0200

    js dateien aufgeräumt

--- src/main/webapp/resources/js/comment.js
+++
@@ -1,9 +0,0 @@
-function deleteComment(commentsId, commentIdex) {
-	var comments = document.getElementById(commentsId);
-	comments.removeChild(comments.childNodes[commentIndex]);
-}
-
-function addComment() {
-	var comments = document.getElementById(commentsId);
-	/* Element aufbauen und anhängen */
-}
